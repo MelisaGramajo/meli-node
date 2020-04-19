cb435dc1a8efc6609fe7a902e8596ba9a51c43f0
MelisaGramajo <melisargramajo@gmail.com>
2020-04-19 13:25:34 -0300

    response returns html

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-    res.write("Hello Word");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>Hello Word con HTML</h1>");
     res.end();
 }).listen(3000, function(){
     console.log("Server listen port 3000")
