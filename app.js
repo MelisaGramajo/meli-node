dc0c93cac62f18229b108412a2698a7df9a36048
MelisaGramajo <melisargramajo@gmail.com>
2020-04-19 14:05:18 -0300

    response returns html+img

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var http = require('http');
 
+
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Hello Word con HTML</h1>");
-    res.end();
+    var url = req.url; 
+    if (url === '/settings') { 
+        res.write ('<h1> MelisaGramajo95 <h1>'); // escribe una respuesta 
+        res.write ('<h3> Gramajo, Melisa Rocio <h3>'); // escribe una respuesta 
+        res.write ('<h3> 24/05/1995 <h3>'); // escribe una respuesta 
+        res.end (); // finaliza la respuesta 
+    } else { 
+        
+        res.write('<img src="img/fotoperfil.png"></a>');
+        res.write (' <h1> Gramajo Melisa Rocio <h1> '); // escribe una respuesta
+        res.end();
+    }
 }).listen(3000, function(){
     console.log("Server listen port 3000")
 
